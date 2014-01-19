2c4f9cb6e62705a73fb3f9c9e9ef7f13ce47ae9c
Lifepillar <github@lifepillar.com>
2014-01-19 23:23:13 +0100

    Update Javascript inclusion.
    See http://foundation.zurb.com/docs/javascript.html:
    
    >[…] In the <head> section of your page add Modernizr.
    >[…] Before the closing body tag, insert JQuery and FastClick.
    >[…] Then either load each plugin individually, or include foundation.min.js, which automatically loads the Foundation Core and all JavaScript plugins.
    
    We include foundation.js instead of foundation.min.js,
    and let nanoc take care of minifying it.

--- content/assets/javascripts/foundation.js
+++ content/assets/javascripts/foundation.js
@@ -1,3 +1,4 @@
+//= require ../../../bower_components/jquery/jquery.js
+//= require ../../../bower_components/fastclick/lib/fastclick.js
 //= require ../../../bower_components/foundation/js/foundation.js
-
 $(document).foundation();
