a9f085d6375d273340251f136ff2921fe0ddabf8
Lifepillar <github@lifepillar.com>
2014-01-19 21:44:21 +0100

    Add a tip about using NANOC_ENV.
    See https://groups.google.com/forum/#!msg/nanoc/Vceo8ducqb0/2FhAWeZL5sMJ and http://clarkdave.net/2012/05/speeding-up-nanoc-compiles/.

--- lib/default.rb
+++ lib/default.rb
@@ -7,3 +7,7 @@ unless defined? LOADED_COMPASS_CONFIG
   Compass.add_project_configuration 'compass/config.rb'
 end
 sass_options = Compass.sass_engine_options
+
+unless ENV['NANOC_ENV']
+  puts "\n\033[1;35mWarn:\033[0m For production build, delete the tmp directory and set NANOC_ENV=production."
+end
