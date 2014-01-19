1276b9a481cf376a1eee528f554b2f1c7b901137
Lifepillar <github@lifepillar.com>
2014-01-19 19:04:45 +0100

    Load Compass in lib/default.rb.
    The conditional may not be needed there, but I
    haven't tested it yet.

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,9 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+unless defined? LOADED_COMPASS_CONFIG
+  LOADED_COMPASS_CONFIG = true
+  require 'compass'
+  Compass.add_project_configuration 'compass/config.rb'
+end
+sass_options = Compass.sass_engine_options
