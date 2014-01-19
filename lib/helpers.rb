a3a3a1898892e442003e01d5cc5cbe4099842cac
Lifepillar <github@lifepillar.com>
2014-01-19 22:19:46 +0100

    Add a helper function to group blog articles.
    Untested.

---
+++ lib/helpers.rb
@@ -0,0 +1,6 @@
+# From http://stackoverflow.com/questions/9538863/using-nanoc-to-create-a-list-of-blog-articles-sorted-by-month-and-year
+def grouped_articles
+  sorted_articles.group_by do |a|
+    [ a[:created_at].year, a[:created_at].month ]
+  end.sort.reverse
+end
