b6888c5d8d58acc289d5faa9b5a58ef652b24fbd
Lifepillar <github@lifepillar.com>
2014-01-21 18:12:04 +0100

    Put blog extensions in a module, and include it.

---
+++ lib/helpers/includes.rb
@@ -0,0 +1,7 @@
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::Tagging
+include Nanoc::Helpers::Rendering
+include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::XMLSitemap
+
+include BloggingExtensions
