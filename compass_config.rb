65329c269dfa0fd8942f7d14e6f8d15094e31828
Lifepillar <github@lifepillar.com>
2015-08-02 13:03:33 +0300

    Update compass_config.rb.

--- compass_config.rb
+++ compass_config.rb
@@ -1,15 +1,31 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+add_import_path "bower_components/foundation/scss"
+add_import_path "bower_components/bootstrap-sass/assets/stylesheets"
+add_import_path "bower_components/fontawesome/scss"
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+http_path             = "/"
+project_path          = File.expand_path(File.dirname(__FILE__))
+css_dir               = "public/css"  # compiled files
+sass_dir              = "content/assets/stylesheets" # source files
+fonts_dir             = "content/assets/fonts"
+images_dir            = "content/assets/images"
+javascripts_dir       = "content/assets/javascripts"
+http_javascripts_path = "javascripts"
+http_stylesheets_path = "css"
+http_images_path      = "images"
+http_fonts_dir        = "fonts"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+# when using SCSS:
+sass_options = {
+  :syntax => :scss
+}
+
+if ENV['NANOC_ENV'] == 'production'
+  output_style = :compressed # :expanded or :nested or :compact or :compressed
+else
+  output_style = :expanded
+end
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -23,3 +39,4 @@ javascripts_dir = "javascripts"
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
