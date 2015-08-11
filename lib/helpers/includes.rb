1d9f063d5a4d99dedb4fc13f9419120663e06c3c
Lifepillar <github@lifepillar.com>
2015-08-11 19:24:50 +0300

    Add helpers for blogging.

--- lib/helpers/includes.rb
+++ lib/helpers/includes.rb
@@ -1,5 +1,9 @@
 include Nanoc::Helpers::HTMLEscape
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::XMLSitemap
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::Tagging
 
+include BloggingExtensions
 
