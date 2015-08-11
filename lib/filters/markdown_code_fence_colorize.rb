48445f31e2f9f5d35ab5764b8fce1bf7aabdef9e
Lifepillar <github@lifepillar.com>
2015-08-11 19:25:42 +0300

    Add filter for MultiMarkdown code blocks.

---
+++ lib/filters/markdown_code_fence_colorize.rb
@@ -0,0 +1,8 @@
+class MarkdownCodeFenceColorize < Nanoc::Filter
+  identifier :markdown_code_fence_colorize_adapter
+
+  def run(content, params = {})
+    content.gsub(/```(?<lang>.+)/, '```language-\k<lang>')
+  end
+end
+
