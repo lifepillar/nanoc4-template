57970d2842b0c77cc8a643a66c623b72537b8e55
Lifepillar <lifepillar@lifepillar.me>
2016-11-19 15:00:22 +0100

    Rename lib/default.rb => lib/requires.rb.

--- lib/default.rb
+++
@@ -1,3 +0,0 @@
-require 'nanoc-external'
-require 'nanoc-git'
-require 'html5small/nanoc'
---
+++ lib/requires.rb
@@ -0,0 +1,3 @@
+require 'nanoc-external'   # To run any external program as a filter
+require 'nanoc-git'        # For deployment via Git
+require 'html5small/nanoc' # To minify HTML5
